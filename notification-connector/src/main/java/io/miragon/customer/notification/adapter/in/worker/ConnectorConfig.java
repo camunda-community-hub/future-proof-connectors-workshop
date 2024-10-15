87f5b600f93bb42d36e854d3513639d86883c0e2
Hafflgav <52525.thomas@gmail.com>
2024-10-15 08:39:47 +0200

    fix: adding component scan

--- notification-connector/src/main/java/io/miragon/customer/notification/adapter/in/worker/ConnectorConfig.java
+++ notification-connector/src/main/java/io/miragon/customer/notification/adapter/in/worker/ConnectorConfig.java
@@ -2,9 +2,11 @@ package io.miragon.customer.notification.adapter.in.worker;
 
 import io.miragon.customer.notification.application.port.in.NotifyCustomerUseCase;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = {"io.miragon"})
 public class ConnectorConfig {
 
     @Bean
--- notification-connector/src/main/java/io/miragon/customer/notification/adapter/in/worker/NotificationConnectorAdapter.java
+++ notification-connector/src/main/java/io/miragon/customer/notification/adapter/in/worker/NotificationConnectorAdapter.java
@@ -1,12 +1,12 @@
 package io.miragon.customer.notification.adapter.in.worker;
 
-
 import io.camunda.connector.api.annotation.OutboundConnector;
 import io.camunda.connector.api.outbound.OutboundConnectorContext;
 import io.camunda.connector.api.outbound.OutboundConnectorFunction;
 import io.camunda.connector.generator.java.annotation.ElementTemplate;
 import io.miragon.customer.notification.application.port.in.NotifyCustomerInCommand;
 import io.miragon.customer.notification.application.port.in.NotifyCustomerUseCase;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,6 +23,7 @@ import org.slf4j.LoggerFactory;
             @ElementTemplate.PropertyGroup(id = "customerMobilePhone", label = "Customer Mobile Phone Number"),
             @ElementTemplate.PropertyGroup(id = "customerEMail", label = "Customer E Mail")
 })
+@RequiredArgsConstructor
 public class NotificationConnectorAdapter implements OutboundConnectorFunction {
     private static final Logger LOGGER = LoggerFactory.getLogger(NotificationConnectorAdapter.class);
 
